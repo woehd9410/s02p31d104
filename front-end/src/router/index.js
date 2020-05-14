19e49440efecdbb6ab131087239ab4a7fe637793
이성준 <dhzm2aud@naver.com>
2020-05-14 18:12:37 +0900

    [Refactor] update router script
    convenient import directory views
    
    Closes S02P31D104-26

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,21 +1,30 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+// Routes
+import paths from "./paths";
+
+function route(path, view, name) {
+  return {
+    name: name || view,
+    path,
+    component: (resolve) => import(`@/views/${view}.vue`).then(resolve),
+  };
+}
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-];
-
+// Create a new router
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes: paths
+    .map((path) => route(path.path, path.view, path.name))
+    .concat([
+      {
+        path: "*",
+        redirect: "/",
+      },
+    ]),
 });
 
 export default router;
