cd518f159c234d3075167b578a53c2f64f2a6cec
박재동 <woehd9410@gmail.com>
2020-05-18 14:13:40 +0900

    [Add] add todolist component & todoModel.js

---
+++ front-end/src/components/todo/FormComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <form v-on:enter.prevent="addTodo">
+    <input type="text" v-model="inputValue" />
+    <button class="btn-reset" v-on:click.prevent="addTodo"></button>
+    <!-- v-on:click => click 이벤트 리슨 (.prevent 속성을 추가하면 이벤트 전파를 중단함) -->
+  </form>
+</template>
+ 
+<script>
+import "@/assets/css/style.css"
+export default {
+  props: ['value'], //부모(App.vue)로부터 받아옴
+  data() {
+    return {
+      inputValue: this.value
+    }
+  },
+  methods: {
+    addTodo() {
+      //$emit => 부모(App.vue)에게 @submit이라는 이름으로 이벤트 전달
+      //this.inputValue 인자로 같이 전달
+      this.$emit('@submit', this.inputValue)
+ 
+      //데이터 추가 후 form을 비우기 위해 inputValue값을 비워줌
+      this.inputValue = ''
+    }
+  }
+}
+</script>
+
---
+++ front-end/src/components/todo/ListComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <ul class="list">
+      <li v-for="(item, index) in data" :key="item.todo">
+        <span class="glyphicon glyphicon-asterisk"></span>
+        {{item.todo}}
+        <span class="todoBtn label label-primary"
+          v-if="selectedTab === 'todo'"
+          v-on:click="finishBtnClick(index)">
+          <span class="glyphicon glyphicon-ok"></span>
+          finish
+        </span>
+        <span class="todoBtn label label-danger"
+          v-if="selectedTab === 'finish'"
+          v-on:click="resetBtnClick(index)">reset</span>
+      </li>
+    </ul>
+  </div>
+ 
+ 
+</template>
+ 
+<script>
+import "@/assets/css/style.css"
+export default {
+  props: ['data', 'selectedTab'],
+  methods: {
+    finishBtnClick(item) {
+      this.$emit('@finish', item)
+    },
+    resetBtnClick(item) {
+      this.$emit('@reset', item)
+    }
+  }
+}
+</script>
+
