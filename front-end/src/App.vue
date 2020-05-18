5268f86e2340dc435efcd23e2a174ec4fba6e02f
이옥주 <dhrwnmc@gmail.com>
2020-05-18 14:41:01 +0900

    remove remark

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,8 +3,6 @@
     <appbar />
     <vview />
     <vfooter />
-
-    <!-- <Login></Login> -->
   </v-app>
 </template>
 
@@ -12,7 +10,6 @@
 import appbar from "@/components/Appbar";
 import vfooter from "@/components/Footer";
 import vview from "@/components/View";
-// import Login from "@/components/Login";
 export default {
   name: "App",
   data() {
