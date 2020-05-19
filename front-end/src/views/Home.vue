9abcbaaa88e0ff04288bf20775a764a3137caefa
박재동 <woehd9410@gmail.com>
2020-05-19 11:27:47 +0900

    [Remove] Remove todolist components Because of complexity to connect backend

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="home">
     <HelloWorld />
-    <TodoList />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-import TodoList from "@/components/todo/TodoList.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
-    TodoList
   },
 };
 </script>
