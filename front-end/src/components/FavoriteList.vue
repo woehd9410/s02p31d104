67a98df8e62d1385c0be69db9d8436b2209e96b5
이성준 <dhzm2aud@naver.com>
2020-05-20 15:45:36 +0900

    [Add] profile sidebar action
    Add list item and search feature
    
    Closes S02P31D104-84

--- front-end/src/components/FavoriteList.vue
+++ front-end/src/components/FavoriteList.vue
@@ -1,33 +1,61 @@
 <template>
   <div>
-    <v-btn
-      block
-      tile
-      color="grey darken-1"
-      class="mb-5"
-      dark
-      @click="addFavorite"
-      ><v-icon>mdi-account-arrow-left-outline </v-icon>&nbsp;Add Favorite</v-btn
-    >
+    <v-dialog v-model="favoriteDialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn block tile color="grey darken-1" class="mb-5" dark v-on="on"
+          ><v-icon>mdi-account-arrow-left-outline </v-icon>&nbsp;Add
+          Favorite</v-btn
+        >
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Add Favorite (Person/Department) </span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  label="User Req Key (ID#Name)"
+                  required
+                ></v-text-field>
+                <v-radio-group v-model="addType" :mandatory="false" row>
+                  <v-radio label="Person" value="Person"></v-radio>
+                  <v-radio label="Department" value="Department"></v-radio>
+                </v-radio-group>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="addFavorite">Add</v-btn>
+          <v-btn color="blue darken-1" text @click="favoriteDialog = false"
+            >Close</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-card max-width="500" class="mx-auto">
-      <v-toolbar color="grey darken-1" dark>
-        <v-toolbar-title>List</v-toolbar-title>
-        <v-spacer></v-spacer>
-
-        <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-      </v-toolbar>
-      <v-list>
-        <v-list-item v-for="item in items" :key="item.title">
-          <v-list-item-avatar>
-            <v-img :src="item.avatar"></v-img>
-          </v-list-item-avatar>
-
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+      <v-text-field
+        solo
+        label="List"
+        append-icon="mdi-magnify"
+        v-model="searchItem"
+        @keyup.enter="searching"
+        :class="{ vibro: notFound }"
+      ></v-text-field>
+      <v-list shaped>
+        <v-list-item-group v-model="selectedPerson" color="primary">
+          <v-list-item v-for="item in showItems" :key="item.title">
+            <v-list-item-avatar>
+              <v-img :src="item.avatar"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
       </v-list>
     </v-card>
   </div>
@@ -35,13 +63,43 @@
 
 <script>
 export default {
+  mounted() {
+    this.showItems = this.items;
+  },
   methods: {
     addFavorite() {
       console.log(`show to add favorite modal`);
+      this.favoriteDialog = false;
+    },
+    searching() {
+      console.log(`search input ${this.searchItem}`);
+      this.showItems = [];
+      if (this.searchItem == "") {
+        this.showItems = this.items;
+        return;
+      }
+
+      for (let item of this.items) {
+        if (item.title.search(this.searchItem) > 0) {
+          this.showItems.push(item);
+          console.log(`add favoriteList ${item.title}`);
+        }
+      }
+      this.notFound = this.showItems.length == 0 ? true : false;
+      this.searchItem = "";
+      setTimeout(() => {
+        this.notFound = false;
+      }, 500);
     },
   },
   data() {
     return {
+      favoriteDialog: false,
+      addType: null,
+      selectedPerson: null,
+      searchItem: null,
+      notFound: false,
+      showItems: [],
       items: [
         {
           icon: true,
