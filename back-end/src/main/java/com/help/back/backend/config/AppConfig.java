e0262f969f15df8a65c306b436867db3cd1e5d75
이응재 <god123564@naver.com>
2020-05-21 13:11:59 +0900

    [ADD] JWT
    close S02P31D104-88

---
+++ back-end/src/main/java/com/help/back/backend/config/AppConfig.java
@@ -0,0 +1,37 @@
+package com.help.back.backend.config;
+
+import javax.sql.DataSource;
+
+import com.help.back.backend.config.security.JwtInterceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@MapperScan(basePackages = "com.help.back.backend.dao")
+@EnableTransactionManagement
+public class AppConfig implements WebMvcConfigurer {
+
+    private static final String[] EXCLUDE_PATHS = {"/swagger-resources/**", "/swagger-ui.html", "/swagger/**",
+            "/error", "/webjars/**", "/swagger-ui.html#/**/**", "/message", "/history", "/file", "/**",
+            "/**/**", "/**/**/**", "/**/**/**/**"
+            };
+
+    @Autowired
+    private JwtInterceptor jwtInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        System.out.println("addInterceptors 실행");
+        registry.addInterceptor(jwtInterceptor).addPathPatterns("/**", "/**/**", "/**/**/**", "/**/**/**/**").excludePathPatterns(EXCLUDE_PATHS);
+    }
+}
