ec2a0d6c0702bc5ef1acd87b4441fe7aa0ab06ea
이응재 <god123564@naver.com>
2020-05-27 12:13:43 +0900

    [ADD] add board crud post, get, delete, update
    close S02P31D104-124, S02P31D104-125, S02P31D104-126, S02P31D104-127

---
+++ back-end/src/main/java/com/help/back/backend/service/BoardService.java
@@ -0,0 +1,13 @@
+package com.help.back.backend.service;
+
+import com.help.back.backend.domain.Board;
+
+import java.util.List;
+
+public interface BoardService {
+    int postBoard(Board board);
+    int deleteBoard(int id);
+    int updateBoard(Board board);
+    List<Board> getAllBoard();
+    Board getBoardById(int id);
+}
