153e7337d9d251e1c827cc4ae6226969bc5be73f
이옥주 <dhrwnmc@gmail.com>
2020-05-27 18:31:04 +0900

    [Update] 수정하기 모달 창

--- front-end/src/components/board/BoardList.vue
+++ front-end/src/components/board/BoardList.vue
@@ -19,52 +19,26 @@
 
             <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field>
 
-            <!-- <v-dialog v-model="dialog" max-width="500px">
-              <template v-slot:activator="{ on }">
-                <v-btn color="primary" dark class="mb-2" v-on="on">New Memo</v-btn>
-              </template>
+            <v-dialog v-model="dialog" max-width="500px">
               <v-card>
                 <v-card-title>
-                  <span class="headline">{{ formTitle }}</span>
+                  <span class="headline">수정하기</span>
                 </v-card-title>
 
                 <v-card-text>
-                  <v-container>
-                    <v-row>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
-                      </v-col>
-                    </v-row>
-                  </v-container>
+                  <writeBoard></writeBoard>
                 </v-card-text>
-
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-                  <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-                </v-card-actions>
               </v-card>
-            </v-dialog> -->
+            </v-dialog>
           </v-toolbar>
         </template>
 
         <template v-slot:item.actions="{ item }">
-          <v-icon small class="mr-2" @click="editItem(item)">
+          <v-icon class="mr-7" @click="editItem(item)">
             mdi-pencil
           </v-icon>
-          <v-icon small @click="deleteItem(item)">
+
+          <v-icon @click="deleteItem(item)" color="red">
             mdi-delete
           </v-icon>
         </template>
@@ -78,7 +52,13 @@
 </template>
 
 <script>
+import writeBoard from "@/components/board/WriteBoard.vue";
+import { EventBus } from "../../plugins/eventBus.js";
 export default {
+  components: {
+    writeBoard,
+  },
+
   data: () => ({
     page: 1,
     itemsPerPage: 7,
@@ -136,6 +116,9 @@ export default {
 
   created() {
     this.initialize();
+    EventBus.$on("use-eventbus", (dialog) => {
+      this.dialog = dialog;
+    });
   },
 
   methods: {
@@ -202,7 +185,7 @@ export default {
 
     deleteItem(item) {
       const index = this.desserts.indexOf(item);
-      confirm("Are you sure you want to delete this item?") && this.desserts.splice(index, 1);
+      confirm("삭제 하시겠습니다?") && this.desserts.splice(index, 1);
     },
 
     close() {
