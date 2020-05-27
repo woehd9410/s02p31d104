cf9422b09ef7070fcc4efc88dddd5c53a694a153
이옥주 <dhrwnmc@gmail.com>
2020-05-28 00:52:16 +0900

    [Update] 글쓰기 버튼, 달력 추가

--- front-end/src/views/sidebar/Board.vue
+++ front-end/src/views/sidebar/Board.vue
@@ -1,15 +1,21 @@
 <template>
-  <v-navigation-drawer
-    :color="themeColor"
-    v-model="$store.state.ui.drawer"
-    app
-    clipped
-  >
+  <v-navigation-drawer :color="themeColor" v-model="$store.state.ui.drawer" app clipped>
+    <v-subheader>Board</v-subheader>
+    <WriteBoardButton />
+
+    <SimpleScheduleCalendar />
+    <v-divider></v-divider>
   </v-navigation-drawer>
 </template>
 
 <script>
+import SimpleScheduleCalendar from "@/components/home/SimpleScheduleCalendar.vue";
+import WriteBoardButton from "@/components/modalButton/WriteBoardButton.vue";
 export default {
+  components: {
+    SimpleScheduleCalendar,
+    WriteBoardButton,
+  },
   computed: {
     themeColor() {
       return this.$store.getters.getThemeColor;
