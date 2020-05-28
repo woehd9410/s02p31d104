8c059bad23f20a31ebbbf8fc742ae71def391cea
이성준 <dhzm2aud@naver.com>
2020-05-28 15:14:34 +0900

    [Add] rest api board axios script
    Add script
    - postBoard
    - searchBoardById
    -  updateBoard
    -  deleteBaord
    -  searchBoardAll
    -  searchBoardByEmail
    
    Closes S02P31D104-136

--- front-end/src/api/axiosScript.js
+++ front-end/src/api/axiosScript.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import userAxiosScript from "@/api/v1/userAxiosScript.js";
 import favoriteAxiosScript from "@/api/v1/favoriteAxiosScript.js";
+import groupAxiosScript from "@/api/v1/groupAxiosScript.js";
+import boardAxiosScript from "@/api/v1/boardAxiosScript.js";
 // 테스트 rest api사용
 const testAxios = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com/users/",
@@ -20,14 +22,40 @@ const getTest = (success, error) => {
 
 const axiosFunction = {
   getTest: (success, error) => getTest(success, error),
-  userSearchByOptions: userAxiosScript.userSearchByOptions,
+
+  // user 
+  searchUserByOptions: userAxiosScript.searchUserByOptions,
   signUp: userAxiosScript.signUp,
   updateUser: userAxiosScript.updateUser,
   kakaoLogin: userAxiosScript.kakaoLogin,
   login: userAxiosScript.login,
+
+  // follow
   follow: favoriteAxiosScript.follow,
   unfollow: favoriteAxiosScript.unfollow,
-  favoriteSearchByToId: favoriteAxiosScript.favoriteSearchByToId,
+  searchFavoriteByToId: favoriteAxiosScript.searchFavoriteByToId,
+
+  // group
+  createGroup: groupAxiosScript.createGroup,
+  searchGroupUserListByGid: groupAxiosScript.searchGroupUserListByGid,
+  deleteGroupById: groupAxiosScript.deleteGroupById,
+  updateGroup: groupAxiosScript.updateGroup,
+  // ----------- group-user
+  addGroupUser: groupAxiosScript.updateGroup,
+  searchGroupByGid: groupAxiosScript.searchGroupByGid,
+  // addGroupUser: groupAxiosScript.updateGroup, // host 변경 api 필요
+  deleteGroupUserByGidAndUid: groupAxiosScript.deleteGroupUserByGidAndUid,
+
+  // board
+  postBoard: boardAxiosScript.postBoard,
+  searchBoardById : boardAxiosScript.searchBoardById,
+  updateBoard : boardAxiosScript.updateBoard,
+  deleteBaord : boardAxiosScript.deleteBaord,
+  searchBoardAll : boardAxiosScript.searchBoardAll,
+  searchBoardByEmail : boardAxiosScript.searchBoardByEmail
+
+  // img
+
 };
 
 export default axiosFunction;
