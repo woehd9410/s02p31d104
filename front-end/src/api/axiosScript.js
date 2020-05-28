f5a7ec3b4c2af321b5fc73634f3514c93217e9a7
이성준 <dhzm2aud@naver.com>
2020-05-28 11:15:55 +0900

    [Add] rest api user axios script
    Add script
    - userSearchByOptions
    - signUp
    - updateUser
    - kakaoLogin
    - login
    
    Closes S02P31D104-134

--- front-end/src/api/axiosScript.js
+++ front-end/src/api/axiosScript.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import http from '@/api/http-common.js';
-
+import userAxiosScript from '@/api/v1/userAxiosScript.js';
 // 테스트 rest api사용
 const testAxios = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com/users/",
@@ -18,56 +17,13 @@ const getTest = (success, error) => {
     });
 };
 
-const getToDo = (param,success, error)=>{
-  http
-    .get(`/api/v1/todo/${param}`)
-    .then((res) => {
-      success(res);
-    })
-    .catch((err) => {
-      error(err);
-    });
-}
-
-const addToDo = (params, success , error) =>{
-  http
-  .post('/api/v1/todo',params)
-  .then((res) => {
-    success(res);
-  })
-  .catch((err) => {
-    error(err);
-  });
-}
-
-const deleteToDo = (param, success, error) =>{
-  http
-  .delete(`/api/v1/todo/${param}`)
-  .then((res) => {
-    success(res);
-  })
-  .catch((err) => {
-    error(err);
-  });
-}
-
-const updateToDo = (params,success, error) =>{
-  http
-  .put('/api/v1/todo', params)
-  .then((res) => {
-    success(res);
-  })
-  .catch((err) => {
-    error(err);
-  });
-}
-
 const axiosFunction = {
   getTest: (success, error) => getTest(success, error),
-  getToDo: (param, success, error) => getToDo(param, success, error),
-  addToDo: (params, success, error) => addToDo(params, success, error),
-  deleteToDo: (param, success, error) => deleteToDo(param, success, error),
-  updateToDo: (params, success, error) => updateToDo(params, success, error),
+  userSearchByOptions: userAxiosScript.userSearchByOptions,
+  signUp: userAxiosScript.signUp,
+  updateUser: userAxiosScript.updateUser,
+  kakaoLogin: userAxiosScript.kakaoLogin,
+  login: userAxiosScript.login,
 };
 
 export default axiosFunction;
---
+++ front-end/src/api/v1/userAxiosScript.js
@@ -0,0 +1,62 @@
+import http from "@/api/v1/http-common.js";
+
+export default {
+  // /api/v1/user? id or name or email = param
+  userSearchByOptions: (param, success, error) => {
+    console.log(`axios in ... user search by options`);
+    let paramType;
+    if (typeof param == "number") paramType = { id: param };
+    else if (param.split("@").length >= 2) paramType = { email: param };
+    else paramType = { name: param };
+    console.log(paramType);
+
+    http
+      .get(`user`, {
+        params: paramType,
+      })
+      .then((res) => {
+        success(res);
+      })
+      .catch((err) => {
+        error(err);
+      });
+  },
+
+  signUp: (param, success, error) => {
+    console.log(`axios in ... sign up`);
+    console.log(param);
+
+    http
+      .post(`user`, param)
+      .then((res) => success(res))
+      .catch((err) => error(err));
+  },
+
+  updateUser: (param, success, error) => {
+    console.log(`axios in ... update user`);
+    console.log(param);
+    http
+      .put(`user`, param)
+      .then((res) => success(res))
+      .catch((err) => error(err));
+  },
+
+  kakaoLogin: (param, success, error) => {
+    console.log(`axios in ... kakao login`);
+    console.log(param);
+    http
+      .get(`user/kakao-login`, { params: { email: param } })
+      .then((res) => success(res))
+      .catch((err) => error(err));
+  },
+
+  login: (param, success, error) => {
+    console.log(`axios in ... login`);
+    console.log(param);
+
+    http
+      .post(`user/login`, param)
+      .then((res) => success(res))
+      .catch((err) => error(err));
+  },
+};
