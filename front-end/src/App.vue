d39cd2e5a171c2caa64a47cce381fd14dcd5ff04
이성준 <dhzm2aud@naver.com>
2020-06-08 23:28:38 +0900

    [Add] 가이드 화면 시작하기 버튼
    - 시작하기 버튼 클릭시 localStorage에 클릭 이벤트 값을 기록하여 서비스 가이드 화면가 한번만 나오도록 수정
    - 서비스 가이드 화면에서 앱으로 이동하는 화면 UX

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <v-app>
-    <appbar />
-    <vview />
-    <vfooter />
+    <div v-if="serviceGuide">
+      <firstGuide @startButtonEvent="serviceStart" />
+      <secondGuide />
+      <thirdGuide />
+      <fourthGuide />
+    </div>
+    <div v-else :class="{ 'fade-in': !serviceGuide }">
+      <appbar />
+      <vview />
+      <vfooter />
+    </div>
   </v-app>
 </template>
 
@@ -10,19 +18,39 @@
 import appbar from "@/components/Appbar";
 import vfooter from "@/components/Footer";
 import vview from "@/components/View";
+import firstGuide from "@/components/guide/Firstguide.vue";
+import secondGuide from "@/components/guide/Secondguide.vue";
+import thirdGuide from "@/components/guide/Thirdguide.vue";
+import fourthGuide from "@/components/guide/Fourthguide.vue";
 export default {
   name: "App",
   data() {
-    return {};
+    return {
+      serviceGuide: true,
+    };
   },
   components: {
     appbar,
     vfooter,
     vview,
+    firstGuide,
+    secondGuide,
+    thirdGuide,
+    fourthGuide,
   },
-  mounted () {
-    this.$store.state.ui.drawer = !(this.$vuetify.breakpoint.xs || this.$vuetify.breakpoint.sm);
+  methods: {
+    serviceStart() {
+      console.log("App serviceStart method");
+      localStorage.setItem("serviceGuide", "already one click event");
+      this.serviceGuide = false;
+    },
+  },
+  mounted() {
+    this.$store.state.ui.drawer = !(
+      this.$vuetify.breakpoint.xs || this.$vuetify.breakpoint.sm
+    );
+    let sg = localStorage.getItem("serviceGuide");
+    this.serviceGuide = sg == null;
   },
 };
 </script>
-
--- front-end/src/components/guide/Firstguide.vue
+++ front-end/src/components/guide/Firstguide.vue
@@ -3,7 +3,7 @@
     <div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
       <div class="w3-display-topleft w3-padding-large w3-xlarge">Logo</div>
       <div class="w3-display-middle">
-        <div class="animation orbit replay">
+        <div class="animation orbit replay" @mouseenter="mouseoverEvent">
           <h1 class="w3-jumbo w3-animate-top">Helpromise</h1>
           <hr class="w3-border-grey" style="margin:auto;width:40%" />
           <p class="w3-large w3-center">team helper</p>
@@ -12,13 +12,15 @@
       <div class="w3-display-bottommiddle w3-padding-large">
         <!-- 버튼 타입1 -->
         <p>
-          <button class="w3-button w3-black">시작하기</button>
-        </p>
-        <!-- 버튼 타입2 -->
-        <p>
-          <button
-            class="w3-button w3-white w3-padding-large w3-large w3-opacity w3-hover-opacity-off"
-          >시작하기</button>
+          <v-btn
+            :class="{ stretch: mouseenterFlag }"
+            class="ma-2"
+            x-large
+            outlined
+            color="white"
+            @click="$emit('startButtonEvent')"
+            >시작하기</v-btn
+          >
         </p>
       </div>
     </div>
@@ -26,7 +28,23 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      mouseenterFlag: false,
+    };
+  },
+  methods: {
+    mouseoverEvent() {
+      console.log("Firstguide mouseoverEvent method");
+
+      this.mouseenterFlag = true;
+      setTimeout(() => {
+        this.mouseenterFlag = false;
+      }, 1500);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
