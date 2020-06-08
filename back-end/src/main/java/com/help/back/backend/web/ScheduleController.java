7c8e5960dd434f79090b2ad6be7c2396488166e5
이성준 <dhzm2aud@naver.com>
2020-06-08 11:20:18 +0900

    [Update] add get ics file module

--- back-end/src/main/java/com/help/back/backend/web/ScheduleController.java
+++ back-end/src/main/java/com/help/back/backend/web/ScheduleController.java
@@ -2,7 +2,9 @@ package com.help.back.backend.web;
 
 import com.help.back.backend.domain.Schedule;
 import com.help.back.backend.domain.User;
+import com.help.back.backend.dto.ResultGroupSchedule;
 import com.help.back.backend.dto.ScheduleDate;
+import com.help.back.backend.module.ICS_File;
 import com.help.back.backend.service.ScheduleService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
@@ -252,62 +254,19 @@ public class ScheduleController {
 
 	@GetMapping("/v1/ics")
     public ResponseEntity ics(@RequestParam String url) {
+        List<Schedule> list =  new ICS_File().getSchedule(url);
+        return new ResponseEntity(list,HttpStatus.OK);
+    }
+
+    @GetMapping("/v1/group/{gid}/schedule")
+    public ResponseEntity searchGroupScheduleByGroupId(@PathVariable int gid){
         try {
-
-            List<Schedule> schedules = new ArrayList<>();
-            BufferedReader br = new BufferedReader(
-                    new InputStreamReader(
-                            new URL(url)
-                                    .openStream()));
-            String str;
-            boolean stFlag = false, etFlag = false, cFlag = false, tFlag = false;
-            String startTime = "", endTime = "", content = "", title = "";
-            while ((str = br.readLine()) != null) {
-                String[] strSplit = str.split(":");
-                if ("DTSTART".equals(strSplit[0])) {
-                    stFlag = true;
-                    if (strSplit.length >= 2) {
-                        String dateStr = strSplit[1];
-                        LocalDateTime ldt = LocalDateTime.of(
-                                Integer.parseInt(dateStr.substring(0, 4)),
-                                Integer.parseInt(dateStr.substring(4, 6)),
-                                Integer.parseInt(dateStr.substring(6, 8)),
-                                Integer.parseInt(dateStr.substring(9, 11)),
-                                Integer.parseInt(dateStr.substring(11, 13)),
-                                Integer.parseInt(dateStr.substring(13, 15))).plusHours(9);
-                        ;
-                        startTime = ldt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-                    }
-                } else if ("DTEND".equals(strSplit[0])) {
-                    etFlag = true;
-                    if (strSplit.length >= 2) {
-                        String dateStr = strSplit[1];
-                        LocalDateTime ldt = LocalDateTime.of(
-                                Integer.parseInt(dateStr.substring(0, 4)),
-                                Integer.parseInt(dateStr.substring(4, 6)),
-                                Integer.parseInt(dateStr.substring(6, 8)),
-                                Integer.parseInt(dateStr.substring(9, 11)),
-                                Integer.parseInt(dateStr.substring(11, 13)),
-                                Integer.parseInt(dateStr.substring(13, 15))).plusHours(9);
-                        ;
-                        endTime = ldt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-                    }
-                } else if ("SUMMARY".equals(strSplit[0])) {
-                    tFlag = true;
-                    if (strSplit.length >= 2) title = strSplit[1];
-                } else if ("DESCRIPTION".equals(strSplit[0])) {
-                    cFlag = true;
-                    if (strSplit.length >= 2) content = strSplit[1];
-                }
-                if (stFlag && etFlag && tFlag && cFlag) {
-                    schedules.add(Schedule.builder().title(title).content(content).startTime(startTime).endTime(endTime).build());
-                    stFlag = etFlag = tFlag = cFlag = false;
-                }
-
-            }
-            return new ResponseEntity(schedules, HttpStatus.OK);
+            List<ResultGroupSchedule> list = scheduleService.searchGroupScheduleByGroupId(gid);
+            return new ResponseEntity(list,HttpStatus.OK);
         }catch (Exception e){
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
     }
+
+
 }
